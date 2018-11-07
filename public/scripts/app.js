6098410914cbef06b8ab3f77e5e94874838b817d
alisa-c <alisachuvilskaya@gmail.com>
2018-11-07 19:37:10 +0000

    updated files after creating function in app.js for the tweets to render

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,87 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (data) => {
+  return `
+      <section class="tweet-container">
+        <article class="tweet">
+          <header>
+            <img src="${data.user.avatars.small}"/>
+            <div class="fullName">${data.user.name}</div>
+            <div class="handle">${data.user.handle}</div>
+          </header>
+          <p class="mainContent">${data.content.text}</p>
+          <footer class="timeStamp">${data.created_at}>
+            <div class="icons">
+              <ion-icon name="flag"></ion-icon>
+              <ion-icon name="repeat"></ion-icon>
+              <ion-icon name="heart"></ion-icon>
+            </div>
+          </footer>
+        <article>
+      </section>
+         `;
+}
+
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+function renderTweets(tweets) {
+  for (tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $('.container').append($tweet);
+  }
+  return;
+}
+
+
+$( document ).ready(function() {
+  renderTweets(data);
+});
+
