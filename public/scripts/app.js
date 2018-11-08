e5ca924c168c3c7d2bd78db43369d0891ea3945e
alisa-c <alisachuvilskaya@gmail.com>
2018-11-08 16:24:14 +0000

    modified the compose tweet section to toggle and added error toggle with tweet input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,7 +13,7 @@ const createTweetElement = (data) => {
             <div class="fullName">${data.user.name}</div>
             <div class="handle">${data.user.handle}</div>
           </header>
-          <p class="mainContent">${data.content.text}</p>
+          <p class="mainContent">${escape(data.content.text)}</p>
           <footer class="timeStamp">${data.created_at}>
             <div class="icons">
               <ion-icon name="flag"></ion-icon>
@@ -34,13 +34,47 @@ function renderTweets(tweets) {
   return;
 }
 
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+   $(document).ready( function () {
+    $(".error").hide();
+          $( "form" ).submit(function( event ) {
+            event.preventDefault();
+             if($(".counter").text() == 140) {
+              $(".error").slideUp();
+              $(".error span").empty();
+              $(".error span").append("<span>Please enter a tweet.</span>");
+              $(".error").slideDown();
+            } else if ($(".counter").text() < 0) {
+              $(".error").slideUp();
+              $(".error span").empty();
+              $(".error").slideDown();
+              $(".error span").append("<span>Your tweet is too long.</span>");
+            } else {
+              $(".error").slideUp();
+              $(".error span").empty();
+                $.ajax({url:'/tweets', method: 'POST', data: $( this ).serialize()})
+                .then(function (data){
+                  location.reload();
+                });
+            }
+          });
+        });
+
+
 $( document ).ready(function() {
   function loadTweet() {
     $.ajax({
       url:' /tweets',
       method: 'GET',
       dataType: 'json',
-      success: renderTweets
+      success: function(data) {
+        renderTweets(data);
+      }
     });
   }
   loadTweet();
